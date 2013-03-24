bd5f58db2883b7dc93c65e04f1e6c462285aaeb8
Aaron Hubbart <aaron.hubbart@gmail.com>
2013-03-24 10:35:42 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FND::Application.config.secret_token = 'fef95e08f9cadcf8a2254c8acbb6b070b710518f02e48337e397112896c25e6e84c246a1715d4f5e423fd019ec8246f58b18f86f4e9716f70eb5f6d5b9284167'

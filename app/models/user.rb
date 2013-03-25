debc08acfc31bf8b2596adc428b63b39b6417e40
Aaron Hubbart <aaron.hubbart@gmail.com>
2013-03-24 21:19:30 -0500

    added user model with additional fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :address_city, 
+  								:address_state, :address_zip, :user_type
   # attr_accessible :title, :body
 end
---
+++ db/migrate/20130325020334_add_fields_to_users.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address_city, :string
+    add_column :users, :address_state, :string
+    add_column :users, :address_zip, :string
+    add_column :users, :user_type, :string
+  end
+end
